f6727c4fc99f610e6bdcf745bfbfc69dae8a26a0
Joe Karlsson <josephcarlson1@gmail.com>
2017-09-13 13:45:43 -0500

    footer Dom TESTS COMPLETE

--- components/Footer/Footer.test.js
+++ components/Footer/Footer.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Footer from './Footer';
 import renderer from 'react-test-renderer';
 
+//Snapshot tests
 test('Footer is rendered', () => {
   const component = renderer.create(
     <footer />
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+// DOM Testing
+test('CheckboxWithLabel changes the text after click', () => {
+  // Render a checkbox with label in the document
+  const footer = shallow(
+    <Footer />
+  );
+
+  console.log('footer: ', footer);
+  expect(footer).toHaveLength(1);
 });
