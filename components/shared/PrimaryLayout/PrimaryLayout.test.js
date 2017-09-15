33a0529ec47d9f0d39e72225314d9393bec0f495
Joe Karlsson <josephcarlson1@gmail.com>
2017-09-15 10:03:49 -0500

    Primary Layout tests complete

--- components/shared/PrimaryLayout/PrimaryLayout.test.js
+++ components/shared/PrimaryLayout/PrimaryLayout.test.js
@@ -16,3 +16,17 @@ test('PrimaryLayout is rendered correctly', () => {
   expect(wrapper).toHaveLength(1);
 });
 
+test('PrimaryLayout should be selectable by the class `PrimaryLayout`', () => {
+  const wrapper = shallow(<MemoryRouter><PrimaryLayout /></MemoryRouter>);
+  expect(wrapper.dive().dive().is('.PrimaryLayout')).toBe(true);
+});
+
+test('PrimaryLayout should mount in the full DOM', () => {
+  const wrapper = shallow(<MemoryRouter><PrimaryLayout /></MemoryRouter>);
+  expect(wrapper.dive().dive().find('.PrimaryLayout').length).toBe(1);
+});
+
+test('PrimaryLayout should render to static HTML', () => {
+  const wrapper = shallow(<MemoryRouter><PrimaryLayout /></MemoryRouter>);
+  expect(wrapper.dive().dive().text()).toContain('<Header /><Switch /><Footer />');
+});
