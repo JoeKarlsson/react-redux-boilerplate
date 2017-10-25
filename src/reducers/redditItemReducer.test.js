8402fabf2a441582feb39a87e7bc9c3a38eca716
Joe Karlsson <josephcarlson1@gmail.com>
2017-10-25 16:00:42 -0500

    updated node version.

--- src/reducers/redditItemReducer.test.js
+++ src/reducers/redditItemReducer.test.js
@@ -2,38 +2,42 @@ import { List } from 'immutable';
 import reducer from './redditItemReducer';
 import * as types from '../constants/actionTypes';
 
-test('redditItem reducer should return the initial state', () => {
-	const initialState = List();
-	expect(reducer(undefined, {})).toEqual(initialState);
-});
+describe('Reddit Item Reducer', () => {
+	describe('initial state', () => {
+		it('should return the initial state', () => {
+			const initialState = List();
+			expect(reducer(undefined, {})).toEqual(initialState);
+		});
+	});
 
-test('redditItem reducer should handle FETCH_POSTS_SUCCESS', () => {
-	const initialState = List();
-	const expectedData = List([{ Post1: 'Lorem' }]);
+	it('should handle FETCH_POSTS_SUCCESS', () => {
+		const initialState = List();
+		const expectedData = List([{ Post1: 'Lorem' }]);
 
-	expect(
-		reducer(initialState, {
-			type: types.FETCH_POSTS_SUCCESS,
-			response: {
-				data: {
-					children: [{ Post1: 'Lorem' }],
+		expect(
+			reducer(initialState, {
+				type: types.FETCH_POSTS_SUCCESS,
+				response: {
+					data: {
+						children: [{ Post1: 'Lorem' }],
+					},
 				},
-			},
-		}),
-	).toEqual(expectedData);
-});
+			}),
+		).toEqual(expectedData);
+	});
 
-test('redditItem reducer should handle REMOVE_ITEM', () => {
-	const mockData = [{ Post1: 'Lorem' }, { Post2: 'Ipsum' }, { Post3: 'Dolors' }];
-	const expectedData = [{ Post1: 'Lorem' }, { Post3: 'Dolors' }];
+	it('should handle REMOVE_ITEM', () => {
+		const mockData = [{ Post1: 'Lorem' }, { Post2: 'Ipsum' }, { Post3: 'Dolors' }];
+		const expectedData = [{ Post1: 'Lorem' }, { Post3: 'Dolors' }];
 
-	const initialState = List(mockData);
-	const index = 1;
+		const initialState = List(mockData);
+		const index = 1;
 
-	expect(
-		reducer(initialState, {
-			type: types.REMOVE_ITEM,
-			index,
-		}),
-	).toEqual(List(expectedData));
+		expect(
+			reducer(initialState, {
+				type: types.REMOVE_ITEM,
+				index,
+			}),
+		).toEqual(List(expectedData));
+	});
 });
